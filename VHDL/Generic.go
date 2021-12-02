af81a065b6097d73a186723c858cd0e168615b3d
RickvanLoo <mail@rickvanloo.nl>
2021-12-02 16:49:13 +0100

    Added Generic VHDL Functions

---
+++ VHDL/Generic.go
@@ -0,0 +1,41 @@
+package VHDL
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+// Generic functions for badmath/VHDL
+
+//TODO: Add funcmap support
+func CreateVHDLFile(FolderPath string, FileName string, TemplateFile string, Data interface{}) {
+	TemplatePath := "template/" + TemplateFile
+
+	path := FolderPath + "/" + FileName
+
+	t, err := template.New(TemplateFile).ParseFiles(TemplatePath)
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	f, err := os.Create(path)
+
+	if err != nil {
+		log.Panicln("Create File: ", err)
+		return
+	}
+
+	err = t.ExecuteTemplate(f, TemplateFile, Data)
+	if err != nil {
+		log.Panicln("Execute: ", err)
+		return
+	}
+}
+
+func FileNameGen(EntityName string) (VHDLFile string, TestFile string) {
+	VHDLFile = EntityName + ".vhd"
+	TestFile = "test_" + EntityName + ".txt"
+	return VHDLFile, TestFile
+}
